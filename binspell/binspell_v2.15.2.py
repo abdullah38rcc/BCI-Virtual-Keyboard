a1b18eb16e0ceee40e06ebf12d22492c3ee89504
spider mumbai <spidermumbai@monmothma.(none)>
2009-11-19 19:43:39 -0800

    * changed window size and textbox size * shuffle-alternate also works with words but a bit slower

--- binspell/binspell_v2.15.2.py
+++ binspell/binspell_v2.15.2.py
@@ -389,49 +389,47 @@ def shuffle(chosen,Nchosen):
 			#print gv._sortByValue(gv._emissionProbs[gv._ngram])
 			#print "-" * 10 
 
-
-
 	set_layout(chosen,gv._emissionProbs[gv._ngram])
 
 
 
+
+#exactly like shuffle except symbols are shuffled every 3 times instead of every time
 def shuffle_alternate(chosen,Nchosen):
 	global gv
 
-	if False:
-		if gv._numTyped > 0:					#sumthin already typed so check for delete
-			gv._box2.remove('[DEL]')		#remove delete
+	gv._emissionProbs[gv._ngram] = updateEmiss(gv._emissionProbs[gv._ngram], chosen)
+	#print "in shuffle: gv._ngram:", gv._ngram
+	#print gv._sortByValue(gv._emissionProbs[gv._ngram])
+	#print "-" * 10 
 
-	gv._currProbs = updateEmiss(chosen,Nchosen,gv._currProbs)
-	gv._currProbs = bg._normalize(gv._currProbs)
-	#compareProbs()				#difference between largest and smallest prob
-	hiProb = getLrgstLeaf(gv._currProbs)
+	hiProb = getLrgstLeaf(gv._emissionProbs[gv._ngram])
 	hiProb = hiProb[0]					####### HACK ########
 	gv._hiProb = hiProb
-	#print "in shuffle: hiProb: ", hiProb
-	#if singleSymbInBx(chosen):
-		#print "eureka!"
-	#if hiProb[1] > gv._threshold or singleSymbInBx(chosen) and hiProb[0] in chosen:			#output a symbol
-	if hiProb[1] > gv._threshold:
-		#condList = list((gv._currProbs[key],key) for key in gv._currProbs)
-		#printProbs(condList)
+
+
+	if hiProb[1] > gv._threshold:			#output a symbol
 		output(hiProb[0])
-		if '[DEL]' in chosen[0]:
+		gv._obsOut.append(hiProb[0])
+		#print "in shuffle: obsout:", gv._obsOut
+		#print #
+		if '[DEL]' in hiProb[0]:
 			#print "deleting"
-			gv._numTyped = gv._numTyped - 1
+			#print "in shuffle: ", chosen[0]
+			#print "numDels in shuffle: ", gv._numDels
 			gv._numDels += 1
-			print "numDels in shuffle: ", gv._numDels
 			return2PrevState()
 			return
 		else:
-			viterbi(gv._obsOut)
-			gv._currProbs = resetConsts(hiProb[0])
+			gv._numB4Shuffle = 3			#so that set_layout is called below
 			saveState()
-			set_layout(chosen,gv._currProbs)
+			#viterbi(gv._obsOut)
+			resetConsts(hiProb[0])
 			infoTransferRate()
-						
+
+
 	if gv._numB4Shuffle == 3:
-		set_layout(chosen,gv._currProbs)
+		set_layout(chosen,gv._emissionProbs[gv._ngram])
 		gv._numB4Shuffle = 0
 	else:
 		gv._numB4Shuffle += 1
@@ -460,8 +458,8 @@ def update(decision):
 		Nchosen = gv._box1
 
 	#split(chosen,Nchosen)	#split symbols like binary search
-	shuffle(chosen,Nchosen)	#shuffle symbols
-	#shuffle_alternate(chosen,Nchosen)
+	#shuffle(chosen,Nchosen)	#shuffle symbols
+	shuffle_alternate(chosen,Nchosen)
 
 	updateCanvas(hilite,norm)
 
@@ -508,13 +506,11 @@ def reWeightDel(eprobs,numwrds,tot):
 
 	
 	
-
-
 #returns: string containing last 2 letters output
 def getBgram():
 	global gv
 	temp = gv._obsOut[-2:]
-	print "in getBgram: temp:", temp
+	#print "in getBgram: temp:", temp
 	return temp[0]+temp[1]
 
 
@@ -599,7 +595,7 @@ def output(item):
 	global gv
 	#global gv._canvas, gv._txtBox
 
-	print "in output(): pos in word: ", gv._posInWrd
+	#print "in output(): pos in word: ", gv._posInWrd
 	#print item
 	#if False:
 	if item == "[SPC]":
@@ -846,7 +842,7 @@ def set_layout(symbs,probs):
 	#splitLaySrtd(symbs,probs)
 	#splitLayHuff(symbs,probs)
 	shuffleHuffLay(probs)
-	#~ shuffleRndmLay(probs)
+	#shuffleRndmLay(probs)
 
 	#print "box1: ", gv._box1
 	gv._box1.sort()
@@ -1150,20 +1146,22 @@ def start():
 ##################################-------------------main---------------########
 
 root = Tk()
+root.config(width=300,height=500)
 gv = GlobalVariables()
 bg = Bigraph()
 tg = Trigraph()
 wd = Words()
 stack = Stack()
 
-canvHeight = 700
-canvWidth = 900
+canvHeight = 600
+canvWidth = 1200
 gv._canHt = canvHeight
 gv._canWdth = canvWidth
-txtBxWidth = canvWidth / 20
+txtBxWidth = canvWidth / 26
 txtBxHeight = 1
 
 gv._canvas = Canvas(root,height=canvHeight,width=canvWidth,bg='yellow')
+gv._canvas.create_window(100,100)
 gv._canvas.grid(row=2,column=1)
 
 #why doesn't tag work?
