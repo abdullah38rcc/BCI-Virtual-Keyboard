65c70e376570809f5942cf720e7cc121f7737533
spider mumbai <spidermumbai@monmothma.(none)>
2010-06-10 00:39:38 -0700

    * sortNumwords.py works *

--- tests/sortNumWords.py
+++ tests/sortNumWords.py
@@ -10,6 +10,37 @@ from operator import itemgetter
 
 def readFile(flname):
 	fd = open(flname, 'r')
-	for line in fd:
+	return fd
+
+def getWords(flhandle):
+	wrdsncnt = {}
+	punct = string.punctuation
+	for line in flhandle:
+		tmp_lst = list(line)
+		for indx, ch in enumerate(tmp_lst):
+			if ch in punct:
+				#print ch
+				del(tmp_lst[indx])
+		word = ''.join(tmp_lst)
+		word = word.lower()
+		wrdsncnt = addtoDict(word,wrdsncnt)
+	return wrdsncnt
+
+def addtoDict(wrd,dictn):
+	if wrd in dictn.keys():
+		dictn[wrd] +=1
+	else:
+		dictn[wrd] = 1
+	return dictn
+
+def printAns(wcdict):
+	print wcdict
+	for key,val in wcdict.items():
+		if val > 1:
+			print "%s %d" %(key, val)
 		
-		
+
+file = 'testWrds.txt'
+flhandler = readFile(file)
+wordCntDict = getWords(flhandler)
+printAns(wordCntDict)
