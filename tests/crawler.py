ef26306094fb7e88ef9ecd588f861cbb0f97f45d
Tamara Blain <embsys@yahoo.com>
2010-06-24 12:52:32 -0700

    * created empty crawler.py * started pyReportTool.py *

---
+++ tests/crawler.py
@@ -0,0 +1,4 @@
+"""
+this is a spider.  it retrieves a web page, grabs the urls, and visits them
+"""
+
---
+++ tests/pyReportTool.py
@@ -0,0 +1,24 @@
+"""
+This script runs a python script and captures its output, compiling it to a pretty report in an html file.
+"""
+
+import subprocess
+
+def openScript(sname):
+	cmd = "python " + sname
+	#child = subprocess.Popen("python hello.py", shell=True)
+	#child = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+	#rslt = child.communicate()[0]
+	rslt = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).communicate()[0]
+	#print rslt
+	return rslt
+	
+def htmlReport():
+	
+	
+
+
+
+fname = "hello.py"
+#fname = 'sortNumWords.py'
+output = openScript(fname)
